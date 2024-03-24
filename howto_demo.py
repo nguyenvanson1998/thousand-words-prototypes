3df0107f750eead70ff03643092d5a73b0c94640
KDR <kevd1337@gmail.com>
2024-03-24 12:01:36 -0700

    demonstrate how to use router query engine to pick between databases

--- howto_demo.py
+++ howto_demo.py
@@ -11,7 +11,6 @@ from llama_index.vector_stores.milvus import MilvusVectorStore
 from pydantic import BaseModel, Field
 import streamlit as st
 
-
 # ===== Query Engine Setup ===== #
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -23,32 +22,36 @@ EMBED_DIMS = 384
 RESPONSE_MODE = "tree_summarize"
 COLLECTION = "howto212k"
 
+
 class HowToStep(BaseModel):
     """How to step data model"""
-    
+
     description: str = Field(
-        description="description of specific step to take in series of steps to solve problem / accomplish task"
+        description=
+        "description of specific step to take in series of steps to solve problem / accomplish task"
     )
-    url: str = Field(
-        description="link to the source youtube video"
-    )
+    url: str = Field(description="link to the source youtube video")
     scene: int = Field(
-        description="relevant video scene number in source video, only provide an integer number"
+        description=
+        "relevant video scene number in source video, only provide an integer number"
     )
     timestamp: int = Field(
-        description="relevant timestamp of scene source video, only provide an integer number"
+        description=
+        "relevant timestamp of scene source video, only provide an integer number"
     )
-    
+
+
 class HowToSteps(BaseModel):
     """Data model for how to step extracted information."""
-    
+
     steps: List[HowToStep] = Field(
-        description="List of steps to take to solve problem / accomplish task"
-    )
+        description="List of steps to take to solve problem / accomplish task")
     summary: str = Field(
-        description="High level summary / commentary on how to solve problem / accomplish. task"
+        description=
+        "High level summary / commentary on how to solve problem / accomplish. task"
     )
-    
+
+
 @st.cache_resource
 def load_query_engine():
     embed_model = HuggingFaceEmbedding(model_name=EMBED)
@@ -58,18 +61,15 @@ def load_query_engine():
     Settings.embed_model = embed_model
     Settings.llm = llm
 
-    vector_store = MilvusVectorStore(
-        dim=EMBED_DIMS,
-        uri=os.getenv('MILVUS_HOST'),
-        token=os.getenv('MILVUS_TOKEN'),
-        overwrite=False,
-        collection_name=COLLECTION
-    )
-    storage_context = StorageContext.from_defaults(
-        vector_store=vector_store, persist_dir=STORAGE
-    )
+    vector_store = MilvusVectorStore(dim=EMBED_DIMS,
+                                     uri=os.getenv('MILVUS_HOST'),
+                                     token=os.getenv('MILVUS_TOKEN'),
+                                     overwrite=False,
+                                     collection_name=COLLECTION)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store,
+                                                   persist_dir=STORAGE)
     index = load_index_from_storage(storage_context=storage_context)
-    
+
     query_engine = index.as_query_engine(
         output_cls=HowToSteps,
         response_mode=RESPONSE_MODE,
@@ -79,8 +79,8 @@ def load_query_engine():
 
     return query_engine, index.index_id
 
-query_engine, index_id = load_query_engine()
 
+query_engine, index_id = load_query_engine()
 
 # =====  Start Main Stream Lit App ===== #
 
@@ -111,24 +111,29 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         if message["role"] == "user":
             st.markdown(message["content"])
-        else:                    
+        else:
             response = message["content"]
             print(response)
             print(type(response.response))
-            st.text("Here's what I found:")            
+            st.text("Here's what I found:")
             if str(type(response.response)) == "<class '__main__.HowToSteps'>":
                 st.markdown(response.response.summary)
                 for no, m in enumerate(response.response.steps):
                     st.header(f'Step {no}')
                     st.markdown(m.description)
                     if m.url:
-                        split = m.url[len('https://www.youtube.com/watch?v='):] + '_split_' + f'{m.scene:05d}'
+                        split = m.url[len('https://www.youtube.com/watch?v='
+                                          ):] + '_split_' + f'{m.scene:05d}'
                         col1, col2 = st.columns(2)
                         with col1:
-                            st.image(f"https://storage.cloud.google.com/kdr-public/pandas70m/howto-travel/img/clip-start/{split}.jpg")
+                            st.image(
+                                f"https://storage.cloud.google.com/kdr-public/pandas70m/howto-travel/img/clip-start/{split}.jpg"
+                            )
                             st.text("clip preview")
                         with col2:
-                            st.video(m.url, format="video/mp4", start_time=m.timestamp)
+                            st.video(m.url,
+                                     format="video/mp4",
+                                     start_time=m.timestamp)
 
             st.divider()
             vs = set([])
@@ -148,20 +153,25 @@ if prompt := st.chat_input("What is up?"):
         response = query_engine.query(prompt)
         print(response)
         print(type(response.response))
-        st.text("Here's what I found:")        
+        st.text("Here's what I found:")
         if str(type(response.response)) == "<class '__main__.HowToSteps'>":
             st.markdown(response.response.summary)
             for no, m in enumerate(response.response.steps):
                 st.header(f'Step {no}')
                 st.markdown(m.description)
                 if m.url:
-                    split = m.url[len('https://www.youtube.com/watch?v='):] + '_split_' + f'{m.scene:05d}'
+                    split = m.url[len('https://www.youtube.com/watch?v='
+                                      ):] + '_split_' + f'{m.scene:05d}'
                     col1, col2 = st.columns(2)
                     with col1:
-                        st.image(f"https://storage.cloud.google.com/kdr-public/pandas70m/howto-travel/img/clip-start/{split}.jpg")
+                        st.image(
+                            f"https://storage.cloud.google.com/kdr-public/pandas70m/howto-travel/img/clip-start/{split}.jpg"
+                        )
                         st.text("clip preview")
                     with col2:
-                        st.video(m.url, format="video/mp4", start_time=m.timestamp)
+                        st.video(m.url,
+                                 format="video/mp4",
+                                 start_time=m.timestamp)
         st.divider()
         vs = set([])
         t = "Sources:\n\n"
@@ -171,4 +181,7 @@ if prompt := st.chat_input("What is up?"):
                 vs.add(s.metadata['video_url'])
         st.markdown(t)
 
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": response
+    })
